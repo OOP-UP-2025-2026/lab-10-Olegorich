97512fbce113a7f87adae5a6b91255e30065792a
Hordiienko Oleh <9292559@stud.op.edu.ua>
2025-11-27 13:30:47 +0200

    Робота виконана за виключенням додаткових завдань

--- app/src/main/java/ua/opnu/EasyTasks.java
+++ app/src/main/java/ua/opnu/EasyTasks.java
@@ -1,6 +1,9 @@
 package ua.opnu;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class EasyTasks {
 
@@ -11,43 +14,60 @@ public class EasyTasks {
     }
 
     public List<Integer> doubling(List<Integer> nums) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        return nums.stream()
+                .map(n -> n * 2)
+                .toList();
     }
 
     public List<Integer> square(List<Integer> nums) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        return nums.stream()
+                .map(n -> n * n)
+                .toList();
     }
 
     public List<String> moreY(List<String> strings) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        return strings.stream()
+                .map(string -> "y" + string + "y")
+                .toList();
     }
 
     public List<Integer> noNeg(List<Integer> nums) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        return nums.stream()
+                .filter(n -> n >= 0)
+                .toList();
     }
 
     public List<Integer> no9(List<Integer> nums) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        return nums.stream()
+                .filter(n -> !(n % 10 == 9))
+                .toList();
     }
 
     public List<String> noZ(List<String> strings) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        return strings.stream()
+                .filter(s -> {
+                    char[] letters = s.toCharArray();
+
+                    for (char letter : letters)
+                        if (Objects.equals(letter, 'z'))
+                            return false;
+
+                    return true;
+                })
+                .toList();
     }
 
     public List<String> refinedStrings(List<String> strings) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        return strings.stream()
+                .distinct()
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .toList();
     }
 
     public List<String> flatten(List<String> strings) {
-        // TODO: напишіть вміст методу згідно умовам для того, щоб пройти тести
-        return null;
+        return strings.stream()
+                .flatMap(str -> Arrays.stream(str.split(" ")))
+                .toList();
     }
 
 }
